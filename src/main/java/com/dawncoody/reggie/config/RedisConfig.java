ba0a01bb08b0b46a19ee81423a228389a883f0d5
dawnCoody <dawntruecool@gmail.com>
2023-04-04 10:58:35 +0800

    Redis环境的配置

---
+++ src/main/java/com/dawncoody/reggie/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.dawncoody.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @description:
+ * @author: dawncoody
+ * @create: 2023-04-04 10:48
+ */
+@Configuration
+public class RedisConfig implements CachingConfigurer {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        // 默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
